caec323e516aa4ee486f30c7c9408be3d069df6c
pike <muenibernice755@gmail.com>
2025-07-16 11:49:47 +0300

    added mueni and BS projects

---
+++ BS/startbootstrap-small-business-gh-pages/JS.js
@@ -0,0 +1,10 @@
+ function myfunction(){
+        // alert("hello,This is an alert")
+        confirm("Are you sure")
+    }
+        function changecolor(){
+        document.getElementById("mybtn").style.backgroundColor="yellow"
+        }
+        function changepara(){
+            document.getElementById("mypara").style.backgroundColor="blue   "
+        }
